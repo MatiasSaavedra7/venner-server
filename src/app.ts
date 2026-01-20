0b76629b39017d618b9c2ca861710a2d69879bae
Matias Saavedra <matiassaavedra1995@gmail.com>
2026-01-20 11:10:30 -0300

    CORS: Actualizar el listado de origenes

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ const app: Application = express();
 
 app.use(
   cors({
-    origin: FRONTEND_URL,
+    origin: [FRONTEND_URL, "http://localhost:5173", "http://127.0.0.1:5173"],
     credentials: true,
   })
 );
