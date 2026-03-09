3b49a56ea279c08cae991a4f2551c9de97efa92e
Matias Saavedra <matiassaavedra1995@gmail.com>
2026-03-09 16:16:08 -0300

    Modifico valor de expiracion del token a 1 dia

--- src/libs/jwt.ts
+++ src/libs/jwt.ts
@@ -3,7 +3,7 @@ import { TOKEN_SECRET } from "../config";
 
 export function createAccessToken(payload: any) {
   return new Promise((resolve, reject) => {
-    jwt.sign(payload, TOKEN_SECRET, { expiresIn: "5min" }, (err, token) => {
+    jwt.sign(payload, TOKEN_SECRET, { expiresIn: "1d" }, (err, token) => {
       if (err) {
         reject(err);
       } else {
